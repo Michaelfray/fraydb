b95be294c4ef93d5793df4eda94ea0e91db701e9
unknown <Michaelfray1@gmail.com>
2024-11-05 12:30:05 +0100

    push up to github

---
+++ ws/Stoorybook3/src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Button = ({ }) => {
+    return (
+        <button>Text</button>
+    )
+}
+
+export default Button;
---
+++ ws/Stoorybook3/src/components/Button/Button.stories.js
@@ -0,0 +1,18 @@
+import Button from './button';
+
+export default {
+    title: " Mike",
+    component: Button,
+}
+
+export const Primary = {
+    args: {
+        primary: true,
+        label: 'Text',
+    },
+};
+
+export const Default = {
+
+}
+
